997eb97f8ed3ad5b674e4906567461edf7d3cf19
HadiARICI <132011080+HadiARICI@users.noreply.github.com>
2023-09-12 23:47:49 +0300

    12.09
    2023

--- B171_TestNG/src/test/java/techproed/utilities/Driver.java
+++ B171_TestNG/src/test/java/techproed/utilities/Driver.java
@@ -3,10 +3,18 @@ package techproed.utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 import java.time.Duration;
 
 public class Driver {
+    private Driver(){
+        /*
+        SINGLETON PATTERN (Tekli kullanım) için gizli const.'ı private yaptık
+         */
+    }
     static WebDriver driver;
     public static WebDriver getDriver(){
         /*
@@ -15,15 +23,38 @@ public class Driver {
         çağrıldığında driver da değer olduğu için direk driver'i return et. Dolayısıyla driver'ı ikinci kez
         çağırdığımızda açık gördüğü browser da yani aynı sayfada belirtilen web sitene gider.
          */
+        /*
+            Testlerimizi farklı driver'larda çalıştırmak için her seferinde getDriver() methodu içindeki
+        driver ayarlarını değiştirmek yerine aşağıdaki gibi switch case ile .properties dosyasında browser
+        olarak belirttiğimiz key'in değeri ne ise browser o driver ile çalışacaktır
+         */
         if (driver==null){
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
+            switch (ConfigReader.getProperty("browser")){
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    break;
+                case "edge":
+                    WebDriverManager.edgedriver().setup();
+                    driver = new EdgeDriver();
+                    break;
+                case "safari":
+                    WebDriverManager.safaridriver().setup();
+                    driver = new SafariDriver();
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    break;
+                default:
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+            }
             driver.manage().window().maximize();
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         }
         return driver;
     }
-
 
     public static void closeDriver(){
         /*
@@ -35,6 +66,12 @@ public class Driver {
          */
         if(driver!=null){
             driver.close();
+            driver = null;
+        }
+    }
+    public static void quitDriver(){
+        if(driver!=null){
+            driver.quit();
             driver = null;
         }
     }
